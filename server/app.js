985040321101c9399c775fa1ef638b0b5686d701
yvlange <79857938+yvlange@users.noreply.github.com>
2021-04-13 18:03:42 +0200

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const db = require("./lib/db");
+
+/*
+  We create an express app calling
+  the express function.
+*/
+const app = express();
+
+/*
+  We setup middleware to:
+  - parse the body of the request to json for us
+  https://expressjs.com/en/guide/using-middleware.html
+*/
+app.use(express.json());
+
+/*
+  Endpoint to handle GET requests to the root URI "/"
+*/
+app.get("/", (req, res) => {
+  res.json({
+    "/posts": "read and create new posts",
+    "/posts/:id": "read, update and delete an individual post",
+  });
+});
+
+/*
+  We have to start the server. We make it listen on the port 4000
+
+*/
+app.listen(4000, () => {
+  console.log("Listening on http://localhost:4000");
+});
